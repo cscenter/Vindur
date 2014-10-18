9ba1d0e1a1e25609e5dbe49f8a38de99951311f7
Sergey Krivohatskiy <s.krivohatskiy@gmail.com>
2014-10-18 14:44:38 +0400

    Different values generating added to OneAttributeTest
    Signed-off-by: Sergey Krivohatskiy <s.krivohatskiy@gmail.com>

--- src/main/java/ru/csc/vindur/test/testHelpers/OneAttributeTest.java
+++ src/main/java/ru/csc/vindur/test/testHelpers/OneAttributeTest.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ThreadLocalRandom;
 
 import ru.csc.vindur.EngineConfig;
 import ru.csc.vindur.Request;
@@ -15,24 +16,25 @@ import ru.csc.vindur.test.RequestGeneratorBase;
 public class OneAttributeTest implements TestHelper {
 	private static final int EXPECTED_VOLUME = 1000000;
 	private static final int REQUESTS_COUNT = 100;
+	private static final Value[] ATTRIBUTE_VALUES = {new Value("first value"), new Value("second value")};
 	private final EngineConfig simpleEngineConfig;
-	private final Map<String, List<Value>> document;
-	private final Request request;
 	
 	public OneAttributeTest() {
 		Map<String, ValueType> indexes = new HashMap<>(1);
 		indexes.put("attribute", ValueType.STRING);
 		simpleEngineConfig = new EngineConfig(indexes, EXPECTED_VOLUME);
-		document = new HashMap<>(1);
-		document.put("attribute", Arrays.asList(new Value("value")));
-		request = Request.build().exact("attribute", "value");
 	}
+
 
 	@Override
 	public DocumentGeneratorBase getDocumentGenerator() {
 		return new DocumentGeneratorBase(false, EXPECTED_VOLUME) {
 			@Override
 			protected Map<String, List<Value>> generateDocument() {
+				Map<String, List<Value>> document = new HashMap<>(1);
+				int idx = ThreadLocalRandom.current().nextInt(ATTRIBUTE_VALUES.length);
+				Value val = ATTRIBUTE_VALUES[idx];
+				document.put("attribute", Arrays.asList(val));
 				return document;
 			}
 		};
@@ -43,6 +45,9 @@ public class OneAttributeTest implements TestHelper {
 		return new RequestGeneratorBase(false, REQUESTS_COUNT) {
 			@Override
 			protected Request generateRequest() {
+				int idx = ThreadLocalRandom.current().nextInt(ATTRIBUTE_VALUES.length);
+				Value val = ATTRIBUTE_VALUES[idx];
+				Request request = Request.build().exact("attribute", val.getValue());
 				return request;
 			}
 		};
@@ -52,6 +57,5 @@ public class OneAttributeTest implements TestHelper {
 	public EngineConfig getEngineConfig() {
 		return simpleEngineConfig;
 	}
-
 
 }
