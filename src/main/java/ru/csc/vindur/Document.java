351a4dc6feba1ba779beb11328ad333c2b39a3cb
Sergey Krivohatskiy <s.krivohatskiy@gmail.com>
2014-10-18 13:22:16 +0400

    Minor fixes in Document class
    Signed-off-by: Sergey Krivohatskiy <s.krivohatskiy@gmail.com>

--- src/main/java/ru/csc/vindur/Document.java
+++ src/main/java/ru/csc/vindur/Document.java
@@ -7,33 +7,33 @@ import java.util.concurrent.atomic.AtomicInteger;
  * Created by Pavel Chursin on 05.10.2014.
  */
 public class Document {
-    private Map<String, List<Value>> vals = new HashMap<>();  // aspect -> values
-    private int id;
+    private final Map<String, List<Value>> vals = new HashMap<>();  // aspect -> values
+    private final int id;
 
     private Document(int id) {
         this.id = id;
     }
 
-    static Document nextDocument(AtomicInteger docSequence) {
+    public static Document nextDocument(AtomicInteger docSequence) {
         return new Document(docSequence.incrementAndGet());
     }
 
-    void setAttribute(String attribute, Value value) {
+    public void setAttribute(String attribute, Value value) {
         if (!vals.containsKey(attribute)) {
             vals.put(attribute, new ArrayList<Value>());
         }
         vals.get(attribute).add(value);
     }
 
-    int getId() {
+    public int getId() {
         return id;
     }
 
-    List<Value> getValues(String attribute) {
+    public List<Value> getValues(String attribute) {
         return vals.get(attribute);
     }
 
-    Set<String> getAttributes() {
+    public Set<String> getAttributes() {
         return vals.keySet();
     }
 }
