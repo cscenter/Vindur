89dd39e3a76c91ee1db8df3fdfefb091f09d3791
Sergey Krivohatskiy <s.krivohatskiy@gmail.com>
2014-11-02 18:27:01 +0300

    EngineConfig rewritten to be thread safe. Old TODO comment removed
    Signed-off-by: Sergey Krivohatskiy <s.krivohatskiy@gmail.com>

--- src/main/java/ru/csc/vindur/EngineConfig.java
+++ src/main/java/ru/csc/vindur/EngineConfig.java
@@ -1,7 +1,10 @@
 package ru.csc.vindur;
 
 import java.util.Map;
-import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+import javax.annotation.concurrent.ThreadSafe;
 
 import ru.csc.vindur.bitset.bitsetFabric.BitSetFabric;
 import ru.csc.vindur.document.StorageType;
@@ -9,19 +12,18 @@ import ru.csc.vindur.document.StorageType;
 /**
  * Created by Pavel Chursin on 05.10.2014.
  */
+@ThreadSafe
 public class EngineConfig {
-    private final Map<String, StorageType> indexes;  // attribute -> value type
+    private final ConcurrentMap<String, StorageType> indexes;  // attribute -> value type
     private final BitSetFabric bitSetFabric;
 
     public EngineConfig(Map<String, StorageType> indexes, BitSetFabric bitSetFabric) {
-        this.indexes = indexes;
+    	synchronized (indexes) {
+            this.indexes = new ConcurrentHashMap<>(indexes);
+		}
         this.bitSetFabric = bitSetFabric;
     }
-
-    public Set<String> getAttributes() {
-        return indexes.keySet();
-    }
-
+    
     public StorageType getValueType(String attribute) {
         return indexes.get(attribute);
     }
--- src/main/java/ru/csc/vindur/document/StorageType.java
+++ src/main/java/ru/csc/vindur/document/StorageType.java
@@ -4,7 +4,6 @@ package ru.csc.vindur.document;
  * Created by Pavel Chursin on 05.10.2014.
  */
 
-//TODO: вообще, это уже скорее StorageType, может так и именовать?
 public enum StorageType {
     STRING,
     ENUM,
