01a37ef7b31f8e9e54f0cf280d83513f2be67e18
Sergey Krivohatskiy <s.krivohatskiy@gmail.com>
2014-11-18 15:07:37 +0300

    Minor refactoring
    Signed-off-by: Sergey Krivohatskiy <s.krivohatskiy@gmail.com>

--- src/test/java/ru/csc/vindur/storage/IntegerRangeStoragesTest.java
+++ src/test/java/ru/csc/vindur/storage/IntegerRangeStoragesTest.java
@@ -15,8 +15,7 @@ import java.util.function.Supplier;
 
 import static org.junit.Assert.assertEquals;
 
-public class IntegerRangeStoragesTest
-{
+public class IntegerRangeStoragesTest {
 	private static final int VALUES_COUNT = 1000;
 	private Supplier<BitSet> bitSetSupplier;
 	private List<Storage> storages;
@@ -29,88 +28,101 @@ public class IntegerRangeStoragesTest
 		storages.add(new StorageIntegers(bitSetSupplier));
 	}
 
-    @Test
-    public void exactRequestTest() throws Exception {
-		for(Storage storage: storages) {
+	@Test
+	public void exactRequestTest() throws Exception {
+		for (Storage storage : storages) {
 			exactRequestTest(storage);
 		}
-    }
+	}
 
 	private void exactRequestTest(Storage storage) throws Exception {
 		fillUpStorage(storage);
 
-        for(int i = 0; i < VALUES_COUNT; i ++) {
-        	assertEquals(bitSetSupplier.get().set(i), ((ExactStorage)storage).findSet(Integer.toString(i)));
-        }
+		for (int i = 0; i < VALUES_COUNT; i++) {
+			assertEquals(bitSetSupplier.get().set(i),
+					((ExactStorage) storage).findSet(Integer.toString(i)));
+		}
 
-        Random random = new Random();
-        
-        for(int i = 0; i < VALUES_COUNT; i ++) {
-        	int match = random.nextInt(VALUES_COUNT);
-        	BitSet expected = bitSetSupplier.get().set(match);
-            assertEquals(expected, ((ExactStorage)storage).findSet(Integer.toString(match)));
-        }
+		Random random = new Random();
+
+		for (int i = 0; i < VALUES_COUNT; i++) {
+			int match = random.nextInt(VALUES_COUNT);
+			BitSet expected = bitSetSupplier.get().set(match);
+			assertEquals(expected,
+					((ExactStorage) storage).findSet(Integer.toString(match)));
+		}
 
 	}
 
 	@Test
 	public void rangeRequestTest() {
-		for(Storage storage: storages) {
+		for (Storage storage : storages) {
 			rangeRequestTest(storage);
 		}
 	}
-	
+
 	private void rangeRequestTest(Storage storage) {
 		fillUpStorage(storage);
 
-        Random random = new Random();
-        
-        for(int i = 0; i < VALUES_COUNT; i ++) {
-        	int match = random.nextInt(VALUES_COUNT);
-        	ROBitSet expected = bitSetSupplier.get().set(match);
-        	ROBitSet actual = ((RangeStorage)storage).findRangeSet(Integer.toString(match), Integer.toString(match));
-            assertEquals(expected, actual);
-        }
+		Random random = new Random();
 
-        for(int i = 0; i < VALUES_COUNT; i ++) {
+		for (int i = 0; i < VALUES_COUNT; i++) {
+			int match = random.nextInt(VALUES_COUNT);
+			ROBitSet expected = bitSetSupplier.get().set(match);
+			ROBitSet actual = ((RangeStorage) storage).findRangeSet(
+					Integer.toString(match), Integer.toString(match));
+			assertEquals(expected, actual);
+		}
+
+		for (int i = 0; i < VALUES_COUNT; i++) {
 			int from = random.nextInt(2 * VALUES_COUNT) - VALUES_COUNT / 2;
 			int to = random.nextInt(2 * VALUES_COUNT) - VALUES_COUNT / 2;
 
 			BitSet expected = bitSetSupplier.get();
-			for(int j = Math.max(0, from); j <= to && j < VALUES_COUNT; j ++) {
+			for (int j = Math.max(0, from); j <= to && j < VALUES_COUNT; j++) {
 				expected = expected.set(j);
 			}
-			assertEquals(expected, ((RangeStorage)storage).findRangeSet(Integer.toString(from), Integer.toString(to)));
-        }
-	}
-
-	private void fillUpStorage(Storage storage) {
-		for(int i = 0; i < VALUES_COUNT; i ++) {
-				storage.add(i, new Value(Integer.toString(i)));
+			assertEquals(
+					expected,
+					((RangeStorage) storage).findRangeSet(
+							Integer.toString(from), Integer.toString(to)));
 		}
 	}
 
-    @Test
-    public void emptyResultTest() throws Exception {
-        for(Storage storage: storages) {
-        	emptyResultTest(storage);
-        }
-    }
-    
-    public void emptyResultTest(Storage storage) throws Exception {
-    	fillUpStorage(storage);
+	private void fillUpStorage(Storage storage) {
+		for (int i = 0; i < VALUES_COUNT; i++) {
+			storage.add(i, new Value(Integer.toString(i)));
+		}
+	}
 
-    	checkForEmptyResult(storage, VALUES_COUNT + 1, VALUES_COUNT + 1);
-    	checkForEmptyResult(storage, VALUES_COUNT, 0);
-    	checkForEmptyResult(storage, -2, -1);
-    	checkForEmptyResult(storage, VALUES_COUNT + 1);
-    	checkForEmptyResult(storage, -1);
-    }
+	@Test
+	public void emptyResultTest() throws Exception {
+		for (Storage storage : storages) {
+			emptyResultTest(storage);
+		}
+	}
 
-    private void checkForEmptyResult(Storage storage, int from, int to) {
-    	assertEquals(0, ((RangeStorage)storage).findRangeSet(Integer.toString(from), Integer.toString(to)).cardinality());
-    }
-    private void checkForEmptyResult(Storage storage, int match) throws Exception {
-    	assertEquals(0, ((ExactStorage)storage).findSet(Integer.toString(match)).cardinality());
-    }
+	public void emptyResultTest(Storage storage) throws Exception {
+		fillUpStorage(storage);
+
+		checkForEmptyResult(storage, VALUES_COUNT + 1, VALUES_COUNT + 1);
+		checkForEmptyResult(storage, VALUES_COUNT, 0);
+		checkForEmptyResult(storage, -2, -1);
+		checkForEmptyResult(storage, VALUES_COUNT + 1);
+		checkForEmptyResult(storage, -1);
+	}
+
+	private void checkForEmptyResult(Storage storage, int from, int to) {
+		assertEquals(
+				0,
+				((RangeStorage) storage).findRangeSet(Integer.toString(from),
+						Integer.toString(to)).cardinality());
+	}
+
+	private void checkForEmptyResult(Storage storage, int match)
+			throws Exception {
+		assertEquals(0,
+				((ExactStorage) storage).findSet(Integer.toString(match))
+						.cardinality());
+	}
 }
--- src/test/java/ru/csc/vindur/storage/StorageLuceneTest.java
+++ src/test/java/ru/csc/vindur/storage/StorageLuceneTest.java
@@ -11,39 +11,43 @@ import ru.csc.vindur.bitset.EWAHBitSet;
 import ru.csc.vindur.document.Value;
 import static org.junit.Assert.assertEquals;
 
-public class StorageLuceneTest
-{
+public class StorageLuceneTest {
 	private StorageLucene storageLucene;
+
 	@Before
 	public void createStorage() {
 		storageLucene = new StorageLucene(EWAHBitSet::new);
 	}
-	
+
 	@Test
 	public void simpleTest() throws ParseException {
 		storageLucene.add(0, new Value("value0"));
 		storageLucene.add(1, new Value("value1"));
 		storageLucene.add(2, new Value("value0 value1"));
-		
+
 		BitSet expected = new EWAHBitSet().set(0).set(2);
-		assertEquals(expected, storageLucene.findSet(new QueryParser("text", new WhitespaceAnalyzer()).parse("value0")));
+		assertEquals(expected, storageLucene.findSet(new QueryParser("text",
+				new WhitespaceAnalyzer()).parse("value0")));
 		expected = new EWAHBitSet().set(1).set(2);
-		assertEquals(expected, storageLucene.findSet(new QueryParser("text", new WhitespaceAnalyzer()).parse("value1")));
+		assertEquals(expected, storageLucene.findSet(new QueryParser("text",
+				new WhitespaceAnalyzer()).parse("value1")));
 	}
-	
+
 	@Test
 	public void wildcardSearchTest() throws ParseException {
 		storageLucene.add(0, new Value("value0"));
-		
+
 		BitSet expected = new EWAHBitSet().set(0);
-		assertEquals(expected, storageLucene.findSet(new QueryParser("text", new WhitespaceAnalyzer()).parse("v?lu*")));
+		assertEquals(expected, storageLucene.findSet(new QueryParser("text",
+				new WhitespaceAnalyzer()).parse("v?lu*")));
 	}
-	
+
 	@Test
 	public void regExpSearchTest() throws ParseException {
 		storageLucene.add(0, new Value("value0"));
-		
+
 		BitSet expected = new EWAHBitSet().set(0);
-		assertEquals(expected, storageLucene.findSet(new QueryParser("text", new WhitespaceAnalyzer()).parse("/[fvs]alue./")));
+		assertEquals(expected, storageLucene.findSet(new QueryParser("text",
+				new WhitespaceAnalyzer()).parse("/[fvs]alue./")));
 	}
 }
